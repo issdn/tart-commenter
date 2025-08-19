666e874b1aaa7faf719d44fb732873c2786fec5b
Karol Bielski <kbi@connext.de>
2025-08-19 15:10:02 +0200

    MOD: Almost complete refactor to use custom dart script instead of spawning analyzer server

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,5 @@
-interface NavigationTarget {
-  kind: string;
-  fileIndex: number;
-  offset: number;
-  length: number;
-  startLine: number;
-  startColumn: number;
-  name: string;
+export type Declaration = {
+  name: string,
+  offset: number,
+  shouldMacro: boolean,
 }
-
-interface NavigationRegion {
-  offset: number;
-  length: number;
-  targets: number[];
-}
-
-interface NavigationResponse {
-  id: string;
-  result?: {
-    files: string[];
-    targets: NavigationTarget[];
-    regions: NavigationRegion[];
-  };
-  error?: {
-    code: string;
-    message: string;
-  };
-}
-
-export type { NavigationTarget, NavigationRegion, NavigationResponse };
